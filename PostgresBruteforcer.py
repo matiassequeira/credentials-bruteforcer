ea60d14e0362aaf412f9fa296c2238e4a7b754b7
Matias Sequeira <matiassequeira@gmail.com>
2020-10-14 12:42:13 -0400

    push

--- PostgresBruteforcer.py
+++ PostgresBruteforcer.py
@@ -5,25 +5,26 @@ host='postgre-db-test.c8ur2wv0ivpl.us-east-1.rds.amazonaws.com'
 users_file='users.txt'
 passwords_file='passwords.txt'
 
-def bruteforce():
-    user_list= open(f'{users_file}',"r")
-    passwords_list= open(f'{passwords_file}',"r")
+user_list= open(f'{users_file}',"r")
+password_list= open(f'{passwords_file}',"r")
 
-    start = time.time()
+start = time.time()
 
-    current_user='postgres'
-    # for current_user in user_list:
-    for current_password in password_list:
+current_user='postgres'
 
-        print(f'Testing password: {current_password}')
+print(f'Testing user: {current_password}')
+# for current_user in user_list:
+for current_password in password_list:
 
-        engine = psycopg2.connect(
-            database="postgres",
-            user=current_user,
-            password=current_password,
-            host=host,
-            port='5432'
-        )
+    print(f'Testing password: {current_password}')
 
-    elapsed = (time.time() - start)
-    print(f'\nBruteforcing time: {elapsed}')
+    engine = psycopg2.connect(
+        database="postgres",
+        user=current_user,
+        password=current_password,
+        host=host,
+        port='5432'
+    )
+
+elapsed = (time.time() - start)
+print(f'\nBruteforcing time: {elapsed}')
